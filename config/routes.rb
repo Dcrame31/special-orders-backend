5a45a8922bcccc86c1f58e6d024b28188119b55e
Diane Crame <dcrame@icloud.com>
2021-05-26 18:17:44 -0700

    add customer controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'customer/name:string'
+  get 'customer/email:string'
+  get 'customer/phone:number'
+  get 'customer/credit_card:number'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
