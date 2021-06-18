dbd9891fa8def9cd092e9259318bf5fdc1120aae
Diane Crame <dcrame@icloud.com>
2021-06-17 20:18:37 -0700

    update customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -6,9 +6,17 @@ class CustomersController < ApplicationController
   end
 
   def show
+    render json: customers
   end
 
   def create
+    customer = Customer.new(customer_params)
+
+    if customer.save
+      render json: customer, except: [:created_at, :updated_at]
+    else
+      render json: customer.errors, status: :unprocessable_entity
+    end
   end
   
   def update
@@ -20,8 +28,10 @@ class CustomersController < ApplicationController
   private
 
   def set_customer
+    customer = Customer.find(params[:id])
   end
 
   def customer_params
+    params.require(:customer).permit(:name, :email, :phone, :credit_card, :user_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :customers
-  resources :users, only: [:create, :update, :destroy, :show]
+  resources :users, only: [:index, :create, :update, :destroy, :show]
   resources :orders 
   resources :items, only: [:index, :create, :update, :destroy, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
